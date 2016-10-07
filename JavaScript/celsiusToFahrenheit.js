c6835425a40ed70432bb6831bd5144ca1a1b3ef2
mauricesandoval <m239111@gmail.com>
2016-10-06 19:32:13 -0700

    Adding files

---
+++ JavaScript/celsiusToFahrenheit.js
@@ -0,0 +1,10 @@
+function convertToF(celsius) {
+  var fahrenheit;
+  
+  fahrenheit = (celsius * (9/5)) + 32; // fahrenheit = celsius * 9/5 + 32 works too.
+  
+  return fahrenheit;
+}
+
+// Change the inputs below to test your code
+convertToF(30);
